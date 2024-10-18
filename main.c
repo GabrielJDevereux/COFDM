a8e50cd8beabe7bc0e36097cf656c01bd39c4783
Gabriel Devereux <gabjol@me.com>
2024-10-18 18:05:37 +1100

    Test

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
+
 #include<stdio.h>
 
-int main(){
-  double age = 0;
-  printf("Enter your age:");
-  scanf("%d",&age);
-  printf("\n%d this is your age.",age);
+include main(){
+  printf("Hello from the other side!");
   return 0;
 }
 
-
-//Test Test Test
-/*
-  BOO
-*/
--- test.c
+++ test.c
@@ -1,21 +1,6 @@
-
 #include<stdio.h>
 
-void sayhi(double hi);
-
 int main(){
-  printf("Hello World");
-  double hi = 1;
-  sayhi(hi)
+  printf("Hi");
   return 0;
 }
-
-void sayhi(double hi){
-  if(hi == 1){
-    
-    printf("hi");
-  }
-}
-
-
-###IS THIS TAKING UPDATE
