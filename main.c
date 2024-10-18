b022c3c4670d7a6761e8a927981fcfc2197ab18c
Gabriel Devereux <gabjol@me.com>
2024-10-18 17:15:33 +1100

    Add More

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+  double age = 0;
+  printf("Enter your age:");
+  scanf("%d",&age);
+  return 0;
+}
+  
--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 
 #include<stdio.h>
 
+void sayhi(double hi);
+
 int main(){
   printf("Hello World");
+  double hi = 1;
+  sayhi(hi)
   return 0;
+}
+
+void sayhi(double hi){
+  if(hi == 1){
+    
+    printf("hi");
+  }
 }
